cdd4a48410c048fef9fcd0fbb538250874766c40
laizige <crl201203@163.com>
2018-08-09 02:44:25 +0800

    完成pos收银demo部分功能

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,42 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <!--左侧导航-->
+    
+        <leftNav></leftNav>
+    
+    <!--操作区域-->
+    <div class="main">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
-
+ 
 <script>
+import leftNav from '@/components/common/leftNav'
 export default {
-  name: 'App'
+  name: 'app',
+  components:{
+    leftNav
+  }
 }
 </script>
-
+ 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Microsoft YaHei','Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
-  margin-top: 60px;
+   height:100%;
+}
+ 
+.main{
+  float:left;
+  width:95%; 
+  background-color: #EFF2F7;
+  height:100%;
+  overflow: auto;
+ 
 }
 </style>
